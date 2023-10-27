89f289e07c76e7472e180dd22ce536e54e70ea3e
olivertms <oliver.tanee@gmail.com>
2023-10-27 15:46:59 +1100

    Initial commit

---
+++ src/main/java/net/nosfe/warcraft/WarcraftClient.java
@@ -0,0 +1,10 @@
+package net.nosfe.warcraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WarcraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
